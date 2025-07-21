eb99d64784bca604facdabf7bb9bbb7d5639c076
Iury Ribeiro <iury211@gmail.com>
2025-07-21 17:27:49 -0300

    Criação do projeto LiterAlura

---
+++ src/main/java/com/literAlura/ExercicioAlura/ExercicioAluraApplication.java
@@ -0,0 +1,13 @@
+package com.literAlura.ExercicioAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/literAlura/ExercicioAlura/ExercicioAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literAlura.ExercicioAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
